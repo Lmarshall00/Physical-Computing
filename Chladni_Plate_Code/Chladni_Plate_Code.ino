5780ceef9b17d8e30a14877ee2fdce32fda5946a
Lmarshall00 <56348067+Lmarshall00@users.noreply.github.com>
2024-05-23 18:59:46 +0100

    Update Chladni_Plate_Code.ino

--- Chladni_Plate_Code/Chladni_Plate_Code.ino
+++ Chladni_Plate_Code/Chladni_Plate_Code.ino
@@ -1,24 +1,5 @@
-/*
-  Pitch follower
-
-  Plays a pitch that changes based on a changing analog input
-
-  circuit:
-  - 8 ohm speaker on digital pin 9
-  - photoresistor on analog 0 to 5V
-  - 4.7 kilohm resistor on analog 0 to ground
-
-  created 21 Jan 2010
-  modified 31 May 2012
-  by Tom Igoe, with suggestion from Michael Flynn
-
-  This example code is in the public domain.
-
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/tonePitchFollower
-*/
-
 void setup() {
-  // initialize serial communications (for debugging only):
+  // initialize serial communications
   Serial.begin(9600);
 }
 
@@ -27,8 +8,7 @@ void loop() {
   int sensorReading = analogRead(A0);
   // print the sensor reading so you know its range
   Serial.println(sensorReading);
-  // map the analog input range (in this case, 400 - 1000 from the photoresistor)
-  // to the output pitch range (120 - 1500Hz)
+
   // change the minimum and maximum input numbers below depending on the range
   // your sensor's giving:
   int thisPitch = map(sensorReading, 0, 1013, 31, 4978);
